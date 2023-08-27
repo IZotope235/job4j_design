3493c945cec117a986c1331428c43cbb1adb5797
iZotope235 <mmnik84@mail.ru>
2023-08-27 21:30:42 +0300

    3. Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,133 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isNotNull()
+                .doesNotContain("Unknown")
+                .startsWith("S")
+                .startsWithIgnoringCase("s")
+                .isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 5);
+        String name = box.whatsThis();
+        assertThat(name).isNotNull()
+                .isNotEmpty()
+                .doesNotContain("Unknown")
+                .startsWith("T")
+                .startsWithIgnoringCase("t")
+                .isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 5);
+        String name = box.whatsThis();
+        assertThat(name).isNotNull()
+                .doesNotContain("Unknown")
+                .startsWith("C")
+                .startsWithIgnoringCase("c")
+                .isEqualTo("Cube");
+    }
+
+    @Test
+    void isThisUnknown() {
+        Box box = new Box(1, 5);
+        String name = box.whatsThis();
+        assertThat(name).isNotNull()
+                .doesNotContain("Sphere", "Tetrahedron", "Cube")
+                .startsWith("U")
+                .isEqualTo("Unknown object");
+    }
+
+    @Test
+    void WhenGetNumberOfVerticesIsFour() {
+        Box box = new Box(4, 5);
+        int numberOfVertices = box.getNumberOfVertices();
+        assertThat(numberOfVertices)
+                .isEven()
+                .isNotNegative()
+                .isNotZero()
+                .isGreaterThan(0)
+                .isEqualTo(4);
+    }
+
+    @Test
+    void WhenGetNumberOfVerticesIsMinusOne() {
+        Box box = new Box(1, 5);
+        int numberOfVertices = box.getNumberOfVertices();
+        assertThat(numberOfVertices)
+                .isOdd()
+                .isNegative()
+                .isNotZero()
+                .isLessThan(0)
+                .isEqualTo(-1);
+    }
+
+    @Test
+    void WhenVertexZeroIsExistTrue() {
+        Box box = new Box(0, 5);
+        boolean isExist = box.isExist();
+        assertThat(isExist)
+                .isNotEqualTo(false)
+                .isTrue();
+    }
+    @Test
+    void WhenVertexFourIsExistTrue() {
+        Box box = new Box(4, 5);
+        boolean isExist = box.isExist();
+        assertThat(isExist).isTrue();
+    }
+
+    @Test
+    void WhenVertexOneIsExistFalse() {
+        Box box = new Box(1, 5);
+        boolean isExist = box.isExist();
+        assertThat(isExist).isFalse();
+    }
+
+    @Test
+    void WhenVertexFourEdgeFiveThenGetArea() {
+        Box box = new Box(4, 5);
+        double area = box.getArea();
+        assertThat(area)
+                .isGreaterThan(43)
+                .isLessThan(44)
+                .isPositive()
+                .isEqualTo(43.3, withPrecision(0.1));
+    }
+
+    @Test
+    void WhenVertexZeroEdgeFiveThenGetArea() {
+        Box box = new Box(0, 5);
+        double area = box.getArea();
+        assertThat(area)
+                .isGreaterThan(314)
+                .isLessThan(315)
+                .isPositive()
+                .isEqualTo(314.15, withPrecision(0.1));
+    }
+
+    @Test
+    void WhenVertexOneEdgeFiveThenZero() {
+        Box box = new Box(1, 5);
+        double area = box.getArea();
+        assertThat(area)
+                .isLessThan(1)
+                .isGreaterThan(-1)
+                .isZero();
+    }
+
+
+
+}
