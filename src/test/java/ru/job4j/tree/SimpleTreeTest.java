d258c9769281e17d0abf2a4851f2f6e342d898ce
iZotope235 <mmnik84@mail.ru>
2023-09-16 22:40:14 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -43,4 +43,26 @@ public class SimpleTreeTest {
         assertThat(tree.add(1, 4)).isTrue();
     }
 
+    @Test
+    void whenIsBinaryTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(3, 5);
+        assertThat(tree.isBinary()).isTrue();
+    }
+
+    @Test
+    void whenIsBinaryFalse() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(2, 6);
+        tree.add(3, 7);
+        assertThat(tree.isBinary()).isFalse();
+    }
+
 }
