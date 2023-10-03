07a5f33a5192f6280ba36fde69c55d044f223926
Marat <mmnik84@mail.ru>
2023-10-03 20:14:30 +0800

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validParameter(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validParameter(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Illegal number of parameters");
+        }
+        if (args[0].equals("null")) {
+            throw new IllegalArgumentException("Root folder is null. Enter root folder path");
+        }
+        if (args[1].equals("null")) {
+            throw new IllegalArgumentException("Search pattern is empty. Enter search pattern like \".js\"");
+        }
     }
 }
