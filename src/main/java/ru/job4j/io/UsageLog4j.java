62d228de8069b952e2f16a6ee2f828d9a28ee504
Marat <mmnik84@mail.ru>
2023-10-10 10:18:50 +0800

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+       String name = "Peter Arsentev";
+        byte age = 33;
+        short weight = 85;
+        int salary = 150000;
+        long numberOfCommits = 4239857394857938457L;
+        boolean isProgrammer = true;
+        float height = 1.81f;
+        double codeLineByDay = 12414423.234234534535d;
+        char group = 'A';
+        LOG.debug("\nUser info name : {}, age : {}, weight : {}, salary : {}, commits : {}, ",
+                name, age, weight, salary, numberOfCommits);
+        LOG.debug("\nIs programmer? : {}, height : {}, code line by day : {}, group : {}",
+                isProgrammer, height, codeLineByDay, group);
+
     }
 }
