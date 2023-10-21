82090521738222d527e106c248a0641672a37e55
Marat <mmnik84@mail.ru>
2023-10-21 23:13:57 +0800

    0. Что такое база данных. [#6860]

---
+++ db/users.sql
@@ -0,0 +1,8 @@
+
+create table users(id serial primary key,name varchar(255), rate int,lastActivity date);
+
+insert into users(name, rate, lastActivity) values('Ivan Ivanov', 5000, '2023-06-30');
+
+update users set rate = 7000;
+
+delete from users;
