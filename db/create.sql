113794793f107b8eccc5915ff279e23327658435
Marat <mmnik84@mail.ru>
2023-10-24 20:42:22 +0800

    2. Схема прав пользователи и роли. [#1093 #455003]

--- db/create.sql
+++ db/create.sql
@@ -1,42 +1,6 @@
-create table users(
-    id serial primary key,
-    name varchar(255)
-);
-
-create table items(
-    id serial primary key,
-    item varchar(255),
-    user_id int references users(id)
-);
-
-create table comments(
-    id serial primary key,
-    comment varchar(255),
-    item_id int references items(id)
-);
-
-create table categories(
-    id serial primary key,
-    name varchar(255),
-    item_id int references items(id)
-);
-
-create table attachs(
-    id serial primary key,
-    name varchar(255),
-    item_id int references items(id)
-);
-
-create table states(
-    id serial primary key,
-    name varchar(255),
-    item_id int references items(id)
-);
-
 create table roles(
     id serial primary key,
-    name varchar(255),
-    user_id int references users(id)
+    name varchar(255)
 );
 
 create table rules(
@@ -49,3 +13,41 @@ create table rules_roles(
     rule_id int references rules(id),
     role_id int references roles(id)
 );
+
+create table users(
+    id serial primary key,
+    name varchar(255),
+    role_id int references roles(id)
+);
+
+create table states(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table categories(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table items(
+    id serial primary key,
+    item varchar(255),
+    user_id int references users(id),
+    category_id int references categories(id),
+    state_id int references states(id)
+);
+
+create table comments(
+    id serial primary key,
+    comment varchar(255),
+    item_id int references items(id)
+);
+
+create table attachs(
+    id serial primary key,
+    name varchar(255),
+    item_id int references items(id)
+);
+
+
--- db/insert.sql
+++ db/insert.sql
@@ -1,19 +1,27 @@
-insert into users(name) values ('Ivan');
-insert into users(name) values ('Peter');
+insert into roles(name) values ('buyer');
+insert into roles(name) values ('customer');
 
-insert into items(item, user_id) values ('lego-34553', 1);
-insert into items(item, user_id) values ('iPhone', 1);
-insert into items(item, user_id) values ('sony-345345', 2);
+insert into rules(name) values ('vip');
+insert into rules(name) values ('regular');
+insert into rules(name) values ('beginner');
 
-insert into states(name, item_id) values ('accepted', 1);
-insert into states(name, item_id) values ('in process', 2);
-insert into states(name, item_id) values ('done', 3);
+insert into rules_roles(rule_id, role_id) values (1, 1);
+insert into rules_roles(rule_id, role_id) values (3, 2);
 
-insert into categories(name, item_id) values ('toys', 1);
-insert into categories(name, item_id) values ('constructor', 1);
-insert into categories(name, item_id) values ('electronics', 2);
-insert into categories(name, item_id) values ('smartphone', 2);
-insert into categories(name, item_id) values ('electronics', 3);
+insert into users(name, role_id) values ('Ivan', 1);
+insert into users(name, role_id) values ('Peter', 2);
+
+insert into states(name) values ('accepted');
+insert into states(name) values ('in process');
+insert into states(name) values ('done');
+
+insert into categories(name) values ('toys');
+insert into categories(name) values ('electronics');
+insert into categories(name) values ('smartphone');
+
+insert into items(item, user_id, category_id, state_id) values ('lego-34553', 1, 1, 2);
+insert into items(item, user_id, category_id, state_id) values ('iPhone', 1, 3, 1);
+insert into items(item, user_id, category_id, state_id) values ('sony-345345', 2, 2, 3);
 
 insert into attachs(name, item_id) values ('lego-view-1.jpg', 1);
 insert into attachs(name, item_id) values ('lego-view-2.jpg', 1);
@@ -27,15 +35,7 @@ insert into comments(comment, item_id) values ('Super item', 2);
 insert into comments(comment, item_id) values ('I love it', 2);
 insert into comments(comment, item_id) values ('I hate it', 3);
 
-insert into roles(name, user_id) values ('buyer', 1);
-insert into roles(name, user_id) values ('customer', 2);
 
-insert into rules(name) values ('vip');
-insert into rules(name) values ('regular');
-insert into rules(name) values ('beginner');
-
-insert into rules_roles(rule_id, role_id) values (1, 1);
-insert into rules_roles(rule_id, role_id) values (3, 2);
 
 
 
