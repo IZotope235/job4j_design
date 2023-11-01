6c417e1e56e584dd52b3b27b778d81f2a8e0f0c3
Marat <mmnik84@mail.ru>
2023-11-01 21:16:12 +0800

    3. Хранимые процедуры и функции [#504804]

---
+++ db/procedure.sql
@@ -0,0 +1,34 @@
+--Добавьте процедуру которая будет удалять записи.
+
+create or replace procedure delete_data(u_id integer)
+language 'plpgsql'
+as $$
+    BEGIN
+		delete from products
+		where id = u_id;
+    END;
+$$;
+
+call delete_date(1);
+
+--Добавьте функцию, которая будет удалять записи.
+create or replace function delete_row(u_id integer)
+returns integer
+language 'plpgsql'
+as
+$$
+    declare
+        result integer;
+		u_count integer;
+    begin
+		select into u_count count from products
+		where id = u_id;
+        if u_count = 0 THEN
+            select into result name from products where id = u_id;
+            delete from products where id = u_id;
+        end if;
+        return result;
+    end;
+$$;
+
+select delete_row(1);
