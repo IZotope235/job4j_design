d6ced445598d3e89d0e2b3d1fce76693ba0afb8d
Marat <mmnik84@mail.ru>
2023-12-06 16:02:18 +0700

    update

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Article {
+    public static boolean generateBy(String text, String sample) {
+        Map<String, String> library = new HashMap<>();
+     return true;
+    }
+}
